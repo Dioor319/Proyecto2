873973eb46af6cdf98992c561832bdbd241654ed
Dioor319 <diego.paesani.pena@est.una.ac.cr>
2026-06-10 16:12:08 -0600

    Add files via upload

---
+++ Proyecto2LorenzoDiego__2-master/cmake-build-debug/CMakeFiles/Proyecto2LorenzoDiego__2.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Enemigo.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Enemigo.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Enemigo.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Entidad.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Entidad.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Entidad.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Juego.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Juego.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Juego.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Jugador.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Jugador.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Jugador.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Mundo.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Mundo.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Mundo.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Objeto.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Objeto.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Objeto.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Registro.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Registro.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Registro.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/Sala.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Sala.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Sala.cpp.obj.d"
+  "C:/Users/Diego Paesani/Downloads/Proyecto2LorenzoDiego__2-master/Proyecto2LorenzoDiego__2-master/main.cpp" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/main.cpp.obj" "gcc" "CMakeFiles/Proyecto2LorenzoDiego__2.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Proyecto2LorenzoDiego__2-master/cmake-build-debug/CMakeFiles/Proyecto2LorenzoDiego__2.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Enemigo.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Enemigo.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Entidad.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Entidad.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Juego.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Juego.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Jugador.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Jugador.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Mundo.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Mundo.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Objeto.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Objeto.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Registro.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Registro.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Sala.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/Sala.cpp.obj.d"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/main.cpp.obj"
+  "CMakeFiles/Proyecto2LorenzoDiego__2.dir/main.cpp.obj.d"
+  "Proyecto2LorenzoDiego__2.exe"
+  "Proyecto2LorenzoDiego__2.exe.manifest"
+  "Proyecto2LorenzoDiego__2.pdb"
+  "libProyecto2LorenzoDiego__2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto2LorenzoDiego__2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
